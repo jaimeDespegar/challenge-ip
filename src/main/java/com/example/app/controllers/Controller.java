2c30bf9a37843f897cb60a408607733c66fc35de
jaimeDespegar <jaime.quispe.1994@outlook.com>
2020-12-16 19:32:26 -0300

    basic code

---
+++ src/main/java/com/example/app/controllers/Controller.java
@@ -0,0 +1,15 @@
+
+package com.example.app.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class Controller {
+
+	@RequestMapping("/")
+	public String index() {
+		return "Hello World!";
+	}
+
+}
---
+++ src/test/java/com/example/app/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.example.app;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class ApplicationTest {
+
+	@Test
+	public void myTest() {
+		Assert.assertTrue(true);
+	}
+
+}
